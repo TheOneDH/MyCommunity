96d43b06006ac2429f261cfab5db6bf6eb35a6c8
dh <704452385@qq.com>
2022-05-03 01:41:40 +0800

    第二章完结

---
+++ src/test/java/com/nowcoder/community/mailTests.java
@@ -0,0 +1,22 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.util.MailClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class mailTests {
+    @Autowired
+    private MailClient mailClient;
+
+    @Test
+    public void testTextMail(){
+        mailClient.sendMail("704452385@qq.com","Test","Welcome.");
+    }
+}
